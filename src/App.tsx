b9b15f6881e04b651a17ff74996964795afd1665
masonlatifprive-crypto <masonlatif.prive@gmail.com>
2026-08-03 09:51:57 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import { TreeLoader } from '@/components/LivingTree'
-import { ToastHost } from '@/components/ui-elite'
-
-// Lazy load components from Layout.tsx
-const LayoutModule = import('@/components/Layout')
-const AppShell = lazy(() => LayoutModule.then(m => ({ default: m.AppShell || m.default })))
-const MarketingShell = lazy(() => LayoutModule.then(m => ({ default: m.MarketingShell || m.default })))
+import { lazy, Suspense, memo } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { AppShell, MarketingShell } from '@/components/Layout';
+import { TreeLoader } from '@/components/LivingTree';
+import { ToastHost } from '@/components/ui-elite';
 
 // Marketing Pages
-const Home = lazy(() => import('@/pages/Home').then(m => ({ default: m.default || m.Home })))
-const Sports = lazy(() => import('@/pages/marketing/Sports').then(m => ({ default: m.default || m.Sports })))
-const Store = lazy(() => import('@/pages/marketing/Store').then(m => ({ default: m.default || m.Store })))
-const Support = lazy(() => import('@/pages/marketing/Support').then(m => ({ default: m.default || m.Support })))
-const FAQ = lazy(() => import('@/pages/marketing/FAQ').then(m => ({ default: m.default || m.FAQ })))
-const Downloads = lazy(() => import('@/pages/marketing/Downloads').then(m => ({ default: m.default || m.Downloads })))
+const Home = lazy(() => import('@/pages/Home').then(m => ({ default: m.Home || m.default })));
+const Sports = lazy(() => import('@/pages/marketing/Sports').then(m => ({ default: m.Sports || m.default })));
+const Store = lazy(() => import('@/pages/marketing/Store').then(m => ({ default: m.Store || m.default })));
+const Support = lazy(() => import('@/pages/marketing/Support').then(m => ({ default: m.Support || m.default })));
+const FAQ = lazy(() => import('@/pages/marketing/FAQ').then(m => ({ default: m.FAQ || m.default })));
+const Downloads = lazy(() => import('@/pages/marketing/Downloads').then(m => ({ default: m.Downloads || m.default })));
 
 // Auth Pages
-const Login = lazy(() => import('@/pages/auth/Login').then(m => ({ default: m.default || m.Login })))
-const Register = lazy(() => import('@/pages/auth/Register').then(m => ({ default: m.default || m.Register })))
-const Subscribe = lazy(() => import('@/pages/auth/Subscribe').then(m => ({ default: m.default || m.Subscribe })))
+const Login = lazy(() => import('@/pages/auth/Login').then(m => ({ default: m.Login || m.default })));
+const Register = lazy(() => import('@/pages/auth/Register').then(m => ({ default: m.Register || m.default })));
+const Subscribe = lazy(() => import('@/pages/auth/Subscribe').then(m => ({ default: m.Subscribe || m.default })));
 
 // App Pages
-const AppHome = lazy(() => import('@/pages/app/AppHome').then(m => ({ default: m.default || m.AppHome })))
-const Discover = lazy(() => import('@/pages/app/Discover').then(m => ({ default: m.default || m.Discover })))
-const Collections = lazy(() => import('@/pages/app/Collections').then(m => ({ default: m.default || m.Collections })))
-const Detail = lazy(() => import('@/pages/app/Detail').then(m => ({ default: m.default || m.Detail })))
-const Player = lazy(() => import('@/pages/app/Player').then(m => ({ default: m.default || m.Player })))
-const NotFoundPage = lazy(() => import('@/pages/NotFound').then(m => ({ default: m.default || m.NotFoundPage })))
+const AppHome = lazy(() => import('@/pages/app/AppHome').then(m => ({ default: m.AppHome || m.default })));
+const Discover = lazy(() => import('@/pages/app/Discover').then(m => ({ default: m.Discover || m.default })));
+const Collections = lazy(() => import('@/pages/app/Collections').then(m => ({ default: m.Collections || m.default })));
+const Detail = lazy(() => import('@/pages/app/Detail').then(m => ({ default: m.Detail || m.default })));
+const Player = lazy(() => import('@/pages/app/Player').then(m => ({ default: m.Player || m.default })));
+const NotFoundPage = lazy(() => import('@/pages/NotFound').then(m => ({ default: m.NotFoundPage || m.default })));
 
 export default function App() {
   return (
@@ -50,22 +46,20 @@ export default function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/subscribe" element={<Subscribe />} />
 
-          {/* Protected App Routes */}
+          {/* App Routes */}
           <Route element={<AppShell />}>
             <Route path="/app" element={<AppHome />} />
             <Route path="/app/discover" element={<Discover />} />
             <Route path="/app/collections" element={<Collections />} />
             <Route path="/app/movie/:id" element={<Detail />} />
             <Route path="/app/tv/:id" element={<Detail />} />
+            <Route path="/app/player/:id" element={<Player />} />
           </Route>
 
-          {/* Player (Full Screen) */}
-          <Route path="/app/player/:id" element={<Player />} />
-
-          {/* 404 */}
+          {/* 404 Route */}
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
-  )
+  );
 }
