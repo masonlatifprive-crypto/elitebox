d01e5e65102c54799fa1f213bb596b7ebc29d823
masonlatifprive-crypto <masonlatif.prive@gmail.com>
2026-08-03 10:01:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { lazy, Suspense, memo } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router';
-import { AppShell, MarketingShell } from '@/components/Layout';
-import { TreeLoader } from '@/components/LivingTree';
-import { ToastHost } from '@/components/ui-elite';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MarketingShell from './components/layout/MarketingShell';
+import AppShell from './components/layout/AppShell';
+import TreeLoader from './components/ui/TreeLoader';
+import { ToastHost } from './components/ui/Toast';
 
 // Marketing Pages
-const Home = lazy(() => import('@/pages/Home').then(m => ({ default: m.Home || m.default })));
-const Sports = lazy(() => import('@/pages/marketing/Sports').then(m => ({ default: m.Sports || m.default })));
-const Store = lazy(() => import('@/pages/marketing/Store').then(m => ({ default: m.Store || m.default })));
-const Support = lazy(() => import('@/pages/marketing/Support').then(m => ({ default: m.Support || m.default })));
-const FAQ = lazy(() => import('@/pages/marketing/FAQ').then(m => ({ default: m.FAQ || m.default })));
-const Downloads = lazy(() => import('@/pages/marketing/Downloads').then(m => ({ default: m.Downloads || m.default })));
+const Home = lazy(() => import('./pages/Home'));
+const Sports = lazy(() => import('./pages/marketing/Sports'));
+const Store = lazy(() => import('./pages/marketing/Store'));
+const Support = lazy(() => import('./pages/marketing/Support'));
+const FAQ = lazy(() => import('./pages/marketing/FAQ'));
+const Downloads = lazy(() => import('./pages/marketing/Downloads'));
 
 // Auth Pages
-const Login = lazy(() => import('@/pages/auth/Login').then(m => ({ default: m.Login || m.default })));
-const Register = lazy(() => import('@/pages/auth/Register').then(m => ({ default: m.Register || m.default })));
-const Subscribe = lazy(() => import('@/pages/auth/Subscribe').then(m => ({ default: m.Subscribe || m.default })));
+const Login = lazy(() => import('./pages/auth/Login'));
+const Register = lazy(() => import('./pages/auth/Register'));
+const Subscribe = lazy(() => import('./pages/auth/Subscribe'));
 
 // App Pages
-const AppHome = lazy(() => import('@/pages/app/AppHome').then(m => ({ default: m.AppHome || m.default })));
-const Discover = lazy(() => import('@/pages/app/Discover').then(m => ({ default: m.Discover || m.default })));
-const Collections = lazy(() => import('@/pages/app/Collections').then(m => ({ default: m.Collections || m.default })));
-const Detail = lazy(() => import('@/pages/app/Detail').then(m => ({ default: m.Detail || m.default })));
-const Player = lazy(() => import('@/pages/app/Player').then(m => ({ default: m.Player || m.default })));
-const NotFoundPage = lazy(() => import('@/pages/NotFound').then(m => ({ default: m.NotFoundPage || m.default })));
+const AppHome = lazy(() => import('./pages/app/AppHome'));
+const Discover = lazy(() => import('./pages/app/Discover'));
+const Collections = lazy(() => import('./pages/app/Collections'));
+const Detail = lazy(() => import('./pages/app/Detail'));
+const Player = lazy(() => import('./pages/app/Player'));
+const NotFoundPage = lazy(() => import('./pages/NotFound'));
 
 export default function App() {
   return (
@@ -51,8 +52,8 @@ export default function App() {
             <Route path="/app" element={<AppHome />} />
             <Route path="/app/discover" element={<Discover />} />
             <Route path="/app/collections" element={<Collections />} />
-            <Route path="/app/movie/:id" element={<Detail />} />
-            <Route path="/app/tv/:id" element={<Detail />} />
+            <Route path="/app/movie/:id" element={<Detail type="movie" />} />
+            <Route path="/app/tv/:id" element={<Detail type="tv" />} />
             <Route path="/app/player/:id" element={<Player />} />
           </Route>
 
