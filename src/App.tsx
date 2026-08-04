8dd6d74ba61c0bddf5196cf1f8645c317771a6f1
masonlatifprive-crypto <masonlatif.prive@gmail.com>
2026-08-04 06:40:53 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,50 @@
 import React, { Suspense, lazy } from 'react';
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
 import MarketingShell from './components/layout/MarketingShell';
-
 import AppShell from './components/layout/AppShell';
-
 import LandingPage from './pages/LandingPage';
-
 import FAQPage from './pages/FAQPage';
-
 import LoadingScreen from './components/ui/LoadingScreen';
 
-
-
 const CollectionsPage = lazy(() => import('./pages/app/CollectionsPage'));
-
 const MovieDetailsPage = lazy(() => import('./pages/app/MovieDetailsPage'));
 
-
-
 function App() {
-  
   return (
-    
     <Router>
-    
       <Routes>
-      
         {/* Marketing Routes */}
-      
-        <Route path=\"/\" element={<MarketingShell />}>
-        
+        <Route path="/" element={<MarketingShell />}>
           <Route index element={<LandingPage />} />
-        
-          <Route path=\"faq\" element={<FAQPage />} />
-        
-        </Route>Route>
-      
+          <Route path="faq" element={<FAQPage />} />
+        </Route>
 
-      
         {/* App Routes */}
-      
-        <Route path=\"/app\" element={<AppShell />}>
-        
-          <Route index element={<Navigate to=\"/app/collections\" replace />} />
-        
+        <Route path="/app" element={<AppShell />}>
+          <Route index element={<Navigate to="/app/collections" replace />} />
           <Route
-            
-            path=\"collections\"
-        
+            path="collections"
             element={
-              
               <Suspense fallback={<LoadingScreen />}>
-              
                 <CollectionsPage />
-              
-              </Suspense>Suspense>
-          
+              </Suspense>
             }
-          
           />
-          
           <Route
-            
-            path=\"movie/:id\"
-          
+            path="movie/:id"
             element={
-              
               <Suspense fallback={<LoadingScreen />}>
-              
                 <MovieDetailsPage />
-              
-              </Suspense>Suspense>
-          
+              </Suspense>
             }
-          
           />
-          
-          </Route>Route>
-          
+        </Route>
 
-          
-            {/* Fallback */}
-          
-        <Route path=\"*\" element={<Navigate to=\"/\" replace />} />
-          
-          </Route>Routes>
-        
-        </Route>Router>
-      
+        {/* Fallback Catch-all */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
-      
-        }
-      
+}
 
-      
-export default App;</Router>
-
+export default App;
