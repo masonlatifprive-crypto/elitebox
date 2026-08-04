94d19309ce0a760b378ceb144ecd7a00609a7d67
masonlatifprive-crypto <masonlatif.prive@gmail.com>
2026-08-04 13:23:43 -0400

    Update separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
 import { cn } from "@/lib/utils"
 
 function Separator({
@@ -10,14 +8,18 @@ function Separator({
   orientation = "horizontal",
   decorative = true,
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: React.HTMLAttributes<HTMLDivElement> & {
+  orientation?: "horizontal" | "vertical"
+  decorative?: boolean
+}) {
   return (
-    <SeparatorPrimitive.Root
+    <div
       data-slot="separator"
-      decorative={decorative}
-      orientation={orientation}
+      role={decorative ? undefined : "separator"}
+      aria-orientation={decorative ? undefined : orientation}
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        "bg-border shrink-0",
+        orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
         className
       )}
       {...props}
