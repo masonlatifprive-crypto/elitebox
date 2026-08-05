a7e735a624b416a7ee7550ad763901c1178a697e
masonlatifprive-crypto <masonlatif.prive@gmail.com>
2026-08-05 02:54:38 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react()],
   server: {
     port: 3000,
@@ -13,5 +13,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    minify: false,
+    sourcemap: true,
+    target: "esnext",
   },
 });
